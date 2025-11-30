c5a6bd14ea42fb1ee00ea8a851c4f39dd173d372
Luiz <ld.andrade@outlook.com>
2025-11-30 05:18:15 +0000

    File changes

--- src/pages/ClientAccess.jsx
+++ src/pages/ClientAccess.jsx
@@ -27,7 +27,7 @@ import {
 } from "lucide-react";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { formatInTimeZone } from "date-fns-tz";
+import { formatLocalDate, formatLocalDateTime } from "@/components/common/DateFormatter";
 import { toast } from "sonner";
 
 export default function ClientAccess() {
