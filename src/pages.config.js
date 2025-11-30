0a5258813a766232ecc071013bfdaaf988c4fbef
Luiz <ld.andrade@outlook.com>
2025-11-30 03:34:13 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import AIAssistant from './pages/AIAssistant';
 import Dashboard from './pages/Dashboard';
 import Clients from './pages/Clients';
-import Cases from './pages/Cases';
 import Documents from './pages/Documents';
 import Templates from './pages/Templates';
 import Tasks from './pages/Tasks';
@@ -29,7 +28,6 @@ export const PAGES = {
     "AIAssistant": AIAssistant,
     "Dashboard": Dashboard,
     "Clients": Clients,
-    "Cases": Cases,
     "Documents": Documents,
     "Templates": Templates,
     "Tasks": Tasks,
