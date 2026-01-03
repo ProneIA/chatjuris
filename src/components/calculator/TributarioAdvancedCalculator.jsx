fd88562172a91b8f6e8866087e2b2097e0d2eab4
Luiz <ld.andrade@outlook.com>
2026-01-03 16:07:43 +0000

    File changes

--- src/components/calculator/TributarioAdvancedCalculator.jsx
+++ src/components/calculator/TributarioAdvancedCalculator.jsx
@@ -3,16 +3,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import RegimeTributarioCalculator from "./RegimeTributarioCalculator";
 import TeseSeculoCalculator from "./TeseSeculoCalculator";
 import ExclusaoISSCalculator from "./ExclusaoISSCalculator";
-import ConsumidorCalculator from "./ConsumidorCalculator";
 
 export default function TributarioAdvancedCalculator({ isDark }) {
   return (
     <Tabs defaultValue="regimes" className="w-full">
-      <TabsList className="grid w-full grid-cols-4">
+      <TabsList className="grid w-full grid-cols-3">
         <TabsTrigger value="regimes">Regimes</TabsTrigger>
         <TabsTrigger value="tese_seculo">Tese do Século</TabsTrigger>
         <TabsTrigger value="exclusao_iss">Exclusão ISS</TabsTrigger>
-        <TabsTrigger value="consumidor">Consumidor</TabsTrigger>
       </TabsList>
       
       <TabsContent value="regimes" className="mt-6">
@@ -25,10 +23,6 @@ export default function TributarioAdvancedCalculator({ isDark }) {
       
       <TabsContent value="exclusao_iss" className="mt-6">
         <ExclusaoISSCalculator isDark={isDark} />
-      </TabsContent>
-      
-      <TabsContent value="consumidor" className="mt-6">
-        <ConsumidorCalculator isDark={isDark} />
       </TabsContent>
     </Tabs>
   );
